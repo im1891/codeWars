f553f28d08f7b9c18f077516cefdff0d8cd3451d
im1891 <im1891@gmail.com>
2022-07-11 20:49:05 +0300

    Complete lesson 05

---
+++ lesson-05.js
@@ -0,0 +1,9 @@
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+    let res = numbers.reduce( (pr, item) => pr + item, 0);
+    return res;
+
+};
+
+//https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
