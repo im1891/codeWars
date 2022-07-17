7b752bf7ef2e357d0d06536a40193914df3a0cbe
im1891 <im1891@gmail.com>
2022-07-17 21:49:35 +0300

    Refactored code

--- lesson-09.js
+++ lesson-09.js
@@ -1,14 +1,12 @@
 function duplicateCount(text){
-    let arr = text.toLowerCase().split('');
-    arr = arr.sort();
+    let arr = text.toLowerCase().split('').sort().filter( (e,i,arr) => e === arr[i+1]).filter( (e,i,arr) => e !== arr[i+1]).length;
 
-    arr = arr.filter( (e,i) => e === arr[i+1]);
 
-    arr= arr.filter( (e,i) => e !== arr[i+1]);
 
-    return arr.length;
+
+
+    return arr;
 
 }
-
 
 //https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/javascript
