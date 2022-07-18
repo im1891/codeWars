756f28f694866a2a8812b396da4a7f50df183491
im1891 <im1891@gmail.com>
2022-07-18 19:23:08 +0300

    Completed lesson 11

---
+++ lesson-11.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+    if (number % 2) {
+        return 'Odd';
+    } else {
+        return 'Even';
+    }
+}
+
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
